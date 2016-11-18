ab2f8449e9ee8e9e5b31bacf176e87d996bd0729
charlesmferguson <charles.m.ferguson.jr@gmail.com>
2016-11-18 15:02:56 -0500

    November 18 class with Andy, working with associations and modeling

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+ServiceOffering.create(name: "Jimmy's Hip-Hop Shop")
+ServiceOffering.create(name: "Emily's Sock Shop")
+ServiceOffering.create(name: "Jon's Productivity Shop")
+ServiceOffering.create(name: "Gabe's Beard Shop")
+ServiceOffering.create(name: "Andy's Rails Shop")
